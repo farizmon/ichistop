2e62eb2f64f4f5b69dffdd4ad69eecf4ca0ba6ff
iferz17@gmail.com <iferz17@gmail.com@415f5784-3fca-2bcb-08c2-4a2c956ac24b>
2014-05-10 16:59:52 +0000

    customer login

---
+++ storecustomer/customer_login.php
@@ -0,0 +1,88 @@
+<?php 
+// This file is www.developphp.com curriculum material
+// Written by Adam Khoury January 01, 2011
+// http://www.youtube.com/view_play_list?p=442E340A42191003
+session_start();
+if (isset($_SESSION["customer"])) {
+    header("location: index.php"); 
+    exit();
+}
+?>
+<?php 
+// Parse the log in form if the user has filled it out and pressed "Log In"
+if (isset($_POST["username"]) && isset($_POST["password"])) {
+
+	$customer = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["username"]); // filter everything but numbers and letters
+    $password = preg_replace('#[^A-Za-z0-9]#i', '', $_POST["password"]); // filter everything but numbers and letters
+    // Connect to the MySQL database  
+    include "../storescripts/connect_to_mysql.php"; 
+    $sql = mysql_query("SELECT id FROM customer WHERE username='$customer' AND password='$password' LIMIT 1"); // query the person
+    // ------- MAKE SURE PERSON EXISTS IN DATABASE ---------
+    $existCount = mysql_num_rows($sql); // count the row nums
+    if ($existCount == 1) { // evaluate the count
+	     while($row = mysql_fetch_array($sql)){ 
+             $id = $row["id"];
+		 }
+		 $_SESSION["id"] = $id;
+		 $_SESSION["customer"] = $customer;
+		 $_SESSION["password"] = $password;
+		 header("location: index.php");
+         exit();
+    } else {
+		echo 'That information is incorrect, try again <a href="index.php">Click Here</a>';
+		exit();
+	}
+}session_destroy();
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Customer Log In </title>
+      <!--<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />-->
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+</head>
+
+<body>
+    <header class="navbar-inverse" role="banner"> 
+      <div align="center" id="mainWrapper" class="container">
+        <?php include_once("template_header5.php");?>
+      <div id="pageContent">
+    </header>
+
+  <div id="pageContent" class= "container"><br />
+    <div align="left" style="margin-left:24px;">
+      <h2>Please Log In</h2>
+      <form id="form1" name="form1" method="post" action="customer_login.php">
+
+          <input name="username" type="text" id="username" size="40" placeholder="username" class="form-control" />
+
+
+       <input name="password" type="password" id="password" size="40" placeholder="password" class="form-control" />
+
+       <br />
+       
+         <input type="submit" name="button" id="button" value="Log In" class="btn btn-primary"/>
+       
+      </form>
+      <p>&nbsp; </p>
+      <a href ="customer_register.php">Register here</a>
+    </div>
+    <br />
+  <br />
+  <br />
+  </div>
+  <?php include_once("template_footer5.php");?>
+</div>
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="../js/bootstrap.min.js"></script>
+</body>
+</html>
