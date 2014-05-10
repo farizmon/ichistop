9f3b1427523d5a5b617026feb385b275ceab3413
iferz17@gmail.com <iferz17@gmail.com@415f5784-3fca-2bcb-08c2-4a2c956ac24b>
2014-05-10 16:47:30 +0000

    cart

---
+++ trunk/cart.php
@@ -0,0 +1,338 @@
+<?php 
+function money_format($format, $number) { 
+    $regex  = '/%((?:[\^!\-]|\+|\(|\=.)*)([0-9]+)?'. 
+              '(?:#([0-9]+))?(?:\.([0-9]+))?([in%])/'; 
+    if (setlocale(LC_MONETARY, 0) == 'C') { 
+        setlocale(LC_MONETARY, ''); 
+		    } 
+			
+    $locale = localeconv(); 
+    preg_match_all($regex, $format, $matches, PREG_SET_ORDER); 
+    foreach ($matches as $fmatch) { 
+        $value = floatval($number); 
+        $flags = array( 
+            'fillchar'  => preg_match('/\=(.)/', $fmatch[1], $match) ? 
+                           $match[1] : ' ', 
+            'nogroup'   => preg_match('/\^/', $fmatch[1]) > 0, 
+            'usesignal' => preg_match('/\+|\(/', $fmatch[1], $match) ? 
+                           $match[0] : '+', 
+            'nosimbol'  => preg_match('/\!/', $fmatch[1]) > 0, 
+            'isleft'    => preg_match('/\-/', $fmatch[1]) > 0 
+        ); 
+
+        $width      = trim($fmatch[2]) ? (int)$fmatch[2] : 0; 
+        $left       = trim($fmatch[3]) ? (int)$fmatch[3] : 0; 
+        $right      = trim($fmatch[4]) ? (int)$fmatch[4] : $locale['int_frac_digits']; 
+        $conversion = $fmatch[5];
+        $positive = true; 
+
+        if ($value < 0) { 
+            $positive = false; 
+            $value  *= -1; 
+        } 
+
+        $letter = $positive ? 'p' : 'n'; 
+        $prefix = $suffix = $cprefix = $csuffix = $signal = ''; 
+        $signal = $positive ? $locale['positive_sign'] : $locale['negative_sign']; 
+
+        switch (true) { 
+            case $locale["{$letter}_sign_posn"] == 1 && $flags['usesignal'] == '+': 
+                $prefix = $signal; 
+                break; 
+
+            case $locale["{$letter}_sign_posn"] == 2 && $flags['usesignal'] == '+': 
+                $suffix = $signal; 
+                break; 
+
+            case $locale["{$letter}_sign_posn"] == 3 && $flags['usesignal'] == '+': 
+                $cprefix = $signal; 
+                break; 
+
+            case $locale["{$letter}_sign_posn"] == 4 && $flags['usesignal'] == '+': 
+                $csuffix = $signal; 
+                break; 
+
+            case $flags['usesignal'] == '(': 
+            case $locale["{$letter}_sign_posn"] == 0: 
+                $prefix = '('; 
+                $suffix = ')'; 
+                break; 
+        } 
+
+        if (!$flags['nosimbol']) { 
+            $currency = $cprefix . 
+                        ($conversion == 'i' ? $locale['int_curr_symbol'] : $locale['currency_symbol']) . 
+                        $csuffix; 
+        } else 
+		{ 
+            $currency = ''; 
+        } 
+
+        $space  = $locale["{$letter}_sep_by_space"] ? ' ' : ''; 
+ 
+        $value = number_format($value, $right, $locale['mon_decimal_point'], 
+
+                 $flags['nogroup'] ? '' : $locale['mon_thousands_sep']); 
+
+        $value = @explode($locale['mon_decimal_point'], $value); 
+
+        $n = strlen($prefix) + strlen($currency) + strlen($value[0]); 
+
+        if ($left > 0 && $left > $n) { 
+            $value[0] = str_repeat($flags['fillchar'], $left - $n) . $value[0]; 
+        } 
+
+        $value = implode($locale['mon_decimal_point'], $value); 
+		
+        if ($locale["{$letter}_cs_precedes"]) { 
+            $value = $prefix . $currency . $space . $value . $suffix; 
+        } else
+		 { 
+            $value = $prefix . $value . $space . $currency . $suffix; 
+        } 
+
+        if ($width > 0) { 
+            $value = str_pad($value, $width, $flags['fillchar'], $flags['isleft'] ? 
+                     STR_PAD_RIGHT : STR_PAD_LEFT); 
+        } 
+
+        $format = str_replace($fmatch[0], $value, $format); 
+    } 
+    return $format; 
+
+} 
+?>
+<?php 
+// This file is www.developphp.com curriculum material
+// Written by Adam Khoury January 01, 2011
+// http://www.youtube.com/view_play_list?p=442E340A42191003
+session_start();
+if (!isset($_SESSION["customer"])) {
+    header("location: storecustomer/customer_login.php"); 
+    exit();
+}
+// Be sure to check that this manager SESSION value is in fact in the database
+$customerID = preg_replace('#[^0-9]#i', '', $_SESSION["id"]); // filter everything but numbers and letters
+$customer = preg_replace('#[^A-Za-z0-9]#i', '', $_SESSION["customer"]); // filter everything but numbers and letters
+$password = preg_replace('#[^A-Za-z0-9]#i', '', $_SESSION["password"]); // filter everything but numbers and letters
+// Run mySQL query to be sure that this person is an admin and that their password session var equals the database information
+// Connect to the MySQL database  
+include "storescripts/connect_to_mysql.php"; 
+$sql = mysql_query("SELECT * FROM customer WHERE id='$customerID' AND username='$customer' AND password='$password' LIMIT 1"); // query the person
+// ------- MAKE SURE PERSON EXISTS IN DATABASE ---------
+$existCount = mysql_num_rows($sql); // count the row nums
+if ($existCount == 0) { // evaluate the count
+     echo "Your login session data is not on record in the database.";
+     exit();
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 1 (if user attempts to add something to the cart from the product page)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_POST['pid'])) {
+    $pid = $_POST['pid'];
+	$wasFound = false;
+	$i = 0;
+	// If the cart session variable is not set or cart array is empty
+	if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) { 
+	    // RUN IF THE CART IS EMPTY OR NOT SET
+		$_SESSION["cart_array"] = array(0 => array("item_id" => $pid, "quantity" => 1));
+	} else {
+		// RUN IF THE CART HAS AT LEAST ONE ITEM IN IT
+		foreach ($_SESSION["cart_array"] as $each_item) { 
+		      $i++;
+		      while (list($key, $value) = each($each_item)) {
+				  if ($key == "item_id" && $value == $pid) {
+					  // That item is in cart already so let's adjust its quantity using array_splice()
+					  array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $pid, "quantity" => $each_item['quantity'] + 1)));
+					  $wasFound = true;
+				  } // close if condition
+		      } // close while loop
+	       } // close foreach loop
+		   if ($wasFound == false) {
+			   array_push($_SESSION["cart_array"], array("item_id" => $pid, "quantity" => 1));
+		   }
+	}
+	header("location: cart.php"); 
+    exit();
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 2 (if user chooses to empty their shopping cart)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_GET['cmd']) && $_GET['cmd'] == "emptycart") {
+    unset($_SESSION["cart_array"]);
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 3 (if user chooses to adjust item quantity)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_POST['item_to_adjust']) && $_POST['item_to_adjust'] != "") {
+    // execute some code
+	$item_to_adjust = $_POST['item_to_adjust'];
+	$quantity = $_POST['quantity'];
+	$quantity = preg_replace('#[^0-9]#i', '', $quantity); // filter everything but numbers
+	if ($quantity >= 100) { $quantity = 99; }
+	if ($quantity < 1) { $quantity = 1; }
+	if ($quantity == "") { $quantity = 1; }
+	$i = 0;
+	foreach ($_SESSION["cart_array"] as $each_item) { 
+		      $i++;
+		      while (list($key, $value) = each($each_item)) {
+				  if ($key == "item_id" && $value == $item_to_adjust) {
+					  // That item is in cart already so let's adjust its quantity using array_splice()
+					  array_splice($_SESSION["cart_array"], $i-1, 1, array(array("item_id" => $item_to_adjust, "quantity" => $quantity)));
+				  } // close if condition
+		      } // close while loop
+	} // close foreach loop
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 4 (if user wants to remove an item from cart)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+if (isset($_POST['index_to_remove']) && $_POST['index_to_remove'] != "") {
+    // Access the array and run code to remove that array index
+ 	$key_to_remove = $_POST['index_to_remove'];
+	if (count($_SESSION["cart_array"]) <= 1) {
+		unset($_SESSION["cart_array"]);
+	} else {
+		unset($_SESSION["cart_array"]["$key_to_remove"]);
+		sort($_SESSION["cart_array"]);
+	}
+}
+?>
+<?php 
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//       Section 5  (render the cart for the user to view on the page)
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+$cartOutput = "";
+$cartTotal = "";
+$pp_checkout_btn = '';
+$product_id_array = '';
+if (!isset($_SESSION["cart_array"]) || count($_SESSION["cart_array"]) < 1) {
+    $cartOutput = "<h2 align='center'>Your shopping cart is empty</h2>";
+} else {
+	// Start PayPal Checkout Button
+	$pp_checkout_btn .= '<form action="https://www.paypal.com/cgi-bin/webscr" method="post">
+    <input type="hidden" name="cmd" value="_cart">
+    <input type="hidden" name="upload" value="1">
+    <input type="hidden" name="business" value="iferz17@gmail.com">';
+	// Start the For Each loop
+	$i = 0; 
+    foreach ($_SESSION["cart_array"] as $each_item) { 
+		$item_id = $each_item['item_id'];
+		$sql = mysql_query("SELECT * FROM products WHERE id='$item_id' LIMIT 1");
+		while ($row = mysql_fetch_array($sql)) {
+			$product_name = $row["product_name"];
+			$price = $row["price"];
+			$details = $row["details"];
+		}
+		$pricetotal = $price * $each_item['quantity'];
+		$cartTotal = $pricetotal + $cartTotal;
+		setlocale(LC_MONETARY, "en_US");
+        $pricetotal = money_format("%10.2n", $pricetotal);
+		// Dynamic Checkout Btn Assembly
+		$x = $i + 1;
+		$pp_checkout_btn .= '<input type="hidden" name="item_name_' . $x . '" value="' . $product_name . '">
+        <input type="hidden" name="amount_' . $x . '" value="' . $price . '">
+        <input type="hidden" name="quantity_' . $x . '" value="' . $each_item['quantity'] . '">  ';
+		// Create the product array variable
+		$product_id_array .= "$item_id-".$each_item['quantity'].","; 
+		// Dynamic table row assembly
+		$cartOutput .= "<tr>";
+		$cartOutput .= '<td><a href="product.php?id=' . $item_id . '">' . $product_name . '</a><br /><img src="inventory_images/' . $item_id . '.jpg" alt="' . $product_name. '" width="40" height="52" border="1" /></td>';
+		$cartOutput .= '<td>' . $details . '</td>';
+		$cartOutput .= '<td>$' . $price . '</td>';
+		$cartOutput .= '<td><form action="cart.php" method="post">
+		<input name="quantity" type="text" value="' . $each_item['quantity'] . '" size="1" maxlength="2" />
+		<input name="adjustBtn' . $item_id . '" type="submit" value="change" />
+		<input name="item_to_adjust" type="hidden" value="' . $item_id . '" />
+		</form></td>';
+		//$cartOutput .= '<td>' . $each_item['quantity'] . '</td>';
+		$cartOutput .= '<td>' . $pricetotal . '</td>';
+		$cartOutput .= '<td><form action="cart.php" method="post"><input name="deleteBtn' . $item_id . '" type="submit" value="X" /><input name="index_to_remove" type="hidden" value="' . $i . '" /></form></td>';
+		$cartOutput .= '</tr>';
+		$i++; 
+    } 
+	setlocale(LC_MONETARY, "en_US");
+    $cartTotal = money_format("%10.2n", $cartTotal);
+	$cartTotal = "<div style='font-size:18px; margin-top:12px;' align='right'>Cart Total : ".$cartTotal." USD</div>";
+    // Finish the Paypal Checkout Btn
+	$pp_checkout_btn .= '<input type="hidden" name="custom" value="' . $product_id_array . '">
+	<input type="hidden" name="notify_url" value="storescripts/my_ipn.php">
+	<input type="hidden" name="return" value="checkout_complete.php">
+	<input type="hidden" name="rm" value="2">
+	<input type="hidden" name="cbt" value="Return to The Store">
+	<input type="hidden" name="cancel_return" value="paypal_cancel.php">
+	<input type="hidden" name="lc" value="US">
+	<input type="hidden" name="currency_code" value="USD">
+	<input type="image" src="http://www.paypal.com/en_US/i/btn/x-click-but01.gif" name="submit" alt="Make payments with PayPal - its fast, free and secure!">
+	</form>';
+}
+?>
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Your Cart</title>
+      <!--<link rel="stylesheet" href="style/style.css" type="text/css" media="screen" />-->
+      <!-- Latest compiled and minified CSS -->
+      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap.min.css">
+
+      <!-- Optional theme -->
+      <link rel="stylesheet" href="//netdna.bootstrapcdn.com/bootstrap/3.1.1/css/bootstrap-theme.min.css">
+</head>
+<body>
+    <header class="navbar-inverse" role="banner"> 
+      <div align="center" id="mainWrapper" class="container">
+        <?php include_once("template_header.php");?>
+      <div id="pageContent">
+    </header>
+  <div id="pageContent">
+    <div style="margin:24px; text-align:left;">
+	
+    <br />
+    <table width="100%" border="1" cellspacing="0" cellpadding="6">
+      <tr>
+        <td width="18%" bgcolor="#C5DFFA"><strong>Product</strong></td>
+        <td width="45%" bgcolor="#C5DFFA"><strong>Product Description</strong></td>
+        <td width="10%" bgcolor="#C5DFFA"><strong>Unit Price</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Quantity</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Total</strong></td>
+        <td width="9%" bgcolor="#C5DFFA"><strong>Remove</strong></td>
+      </tr>
+     <?php echo $cartOutput; ?>
+     <!-- <tr>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+        <td>&nbsp;</td>
+      </tr> -->
+    </table>
+    <?php echo $cartTotal; ?>
+    <br />
+<br />
+<?php echo $pp_checkout_btn; ?>
+    <br />
+    <br />
+    <a href="cart.php?cmd=emptycart">Click Here to Empty Your Shopping Cart</a>
+    </div>
+   <br />
+  </div>
+  <?php include_once("template_footer.php");?>
+</div>
+
+    <!-- Latest compiled and minified JavaScript -->
+    <script src="//netdna.bootstrapcdn.com/bootstrap/3.1.1/js/bootstrap.min.js"></script>
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+</body>
+</html>
