de829b422be0992f63439e8e5ecc955e6a49a7e1
iferz17@gmail.com <iferz17@gmail.com@415f5784-3fca-2bcb-08c2-4a2c956ac24b>
2014-05-10 16:54:00 +0000

    template_header3

---
+++ trunk/storevendor/template_header3.php
@@ -0,0 +1,38 @@
+<div id="pageHeader">
+		<nav role="navigation">
+		  <div class="container-fluid">
+		    <!-- Brand and toggle get grouped for better mobile display -->
+		    <div class="navbar-header">
+		      <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+		        <span class="sr-only">Toggle navigation</span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		        <span class="icon-bar"></span>
+		      </button>
+		      <a class="navbar-brand" href="../index.php">Home</a>
+		    </div>
+
+		    <!-- Collect the nav links, forms, and other content for toggling -->
+		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+		      	<ul class="nav navbar-nav">
+		       	 <li><a href="../product_list.php">Product</a></li>
+		       	 <li><a href="#">Help</a></li>
+		       	 <li><a href="#">Contact</a></li>
+
+   		        </ul>
+		    	<ul class="nav navbar-nav navbar-right">
+		        <li class="dropdown">
+		          <a href="#" class="dropdown-toggle" data-toggle="dropdown">Login <b class="caret"></b></a>
+		          <ul class="dropdown-menu">
+		            <li><a href="../storecustomer/">Customer</a></li>
+		            <li><a href="../storevendor/">Vendor</a></li>
+		            <li><a href="../storeadmin/">Admin</a></li>
+		            <li class="divider"></li>
+		            <li><a href="vendor_logout.php">Log Out</a></li>
+		          </ul>
+		        </li>
+		      </ul>
+		    </div><!-- /.navbar-collapse -->
+		  </div><!-- /.container-fluid -->
+		</nav>
+</div>
